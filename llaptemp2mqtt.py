157d400370688f99e25ec376cde16cd659fc15a7
jfarcher <jf_archer@yahoo.com>
2014-09-18 14:07:04 +0000

    Removing irrelevant parts and added loop to remove trailing - characters from the string without effecting a potential negative value.

--- llaptemp2mqtt.py
+++ llaptemp2mqtt.py
@@ -5,13 +5,11 @@ import time
 import serial
 import datetime
 import re
-#import eeml
 
-broker = "127.0.0.1"
+broker = "192.168.1.3"
 tcpport = 1883
-baud = 9600
-port = '/dev/pts/4'
-#port = '/dev/ttyAMA0'
+baud = 115200
+port = '/dev/ttyAMA0'
 ser = serial.Serial(port, baud)
 
 mypid = os.getpid()
@@ -24,21 +22,9 @@ mqttc.connect(broker, tcpport, 60, True)
 #Remain connected and publish
 while mqttc.loop() == 0:
         llapMsg = ser.read(12)
-	#llapMsg = "aA1TEMP13---"
-	print llapMsg
-        #mqttc.publish("TempSensors", llapMsg)
         devID = llapMsg[1:3]
+        temp = llapMsg[7:12]
+	while temp.endswith("-"):
+			temp = temp[:-1]
+	mqttc.publish("house/temp/" + devID,temp)
 
-        if re.search("aA1TEMP", llapMsg):
-                temp = llapMsg[7:12]
-		mqttc.publish(devID + "TempSensor",temp)
-
-        if re.search("aA2TEMP", llapMsg):
-                temp = llapMsg[7:12]
-		mqttc.publish(devID + "TempSensor",temp)
-
-        #Push data to XIVELY
-        try:
-		print llapMsg
-        except:
-                print("ERROR : Failed to Send...")
