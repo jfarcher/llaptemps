6eff0be3d7ef768698d3b12642091f3102b20151
jfarcher <jf_archer@yahoo.com>
2014-09-23 14:30:48 +0000

    Added string check to ensure we are passing the correct data not just the entire payload.
    Also added clean exit when ctrl-c

--- llaptemp2mqtt.py
+++ llaptemp2mqtt.py
@@ -6,6 +6,13 @@ import time
 import serial
 import datetime
 import re
+import sys
+import signal
+
+def signal_handler(signal, frame):
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 broker = "192.168.1.3"
 tcpport = 1883
@@ -20,13 +27,17 @@ mqttc = mqtt.Client()
 
 #Connect to broker
 #mqttc.connect(broker, tcpport, 60, True)
-mqttc.connect (broker)
+mqttc.connect (broker, tcpport, 60)
 #Remain connected and publish
-while mqttc.loop() == 0:
-        llapMsg = ser.read(12)
+#while mqttc.loop() == 0:
+mqttc.loop_start()
+while True:
+	llapMsg = ser.read(12)
         devID = llapMsg[1:3]
+	sensor = llapMsg[3:7]
         temp = llapMsg[7:12]
 	while temp.endswith("-"):
 			temp = temp[:-1]
-	mqttc.publish("house/temp/" + devID,temp, retain=True)
-
+	if sensor == "TEMP":
+		mqttc.publish("house/temp/" + devID,temp, retain=True)
+mqttc.loop_forever()
