1b30c13fed8e26915f3b28eb7f905ed24df7cfc1
jfarcher <jf_archer@yahoo.com>
2014-09-24 00:57:13 +0100

    Changed to use url instead of file, that way it can be hosted

--- weathermqtt.py
+++ weathermqtt.py
@@ -6,7 +6,9 @@ import time
 import re
 import sys
 import signal
+import urllib
 import json
+
 
 def signal_handler(signal, frame):
     sys.exit(0)
@@ -16,19 +18,20 @@ signal.signal(signal.SIGINT, signal_handler)
 broker = "192.168.1.3"
 tcpport = 1883
 topic = "house/temp/outside"
-file="/tmp/current-weather.json"
+url = "http://jonarcher.info/weather-data"
 
-mypid = os.getpid()
-client_uniq = "pubclient_"+str(mypid)
 mqttc = mqtt.Client()
-
 #Connect to broker
 #mqttc.connect(broker, tcpport, 60, True)
 mqttc.connect (broker, tcpport, 60)
 mqttc.loop_start()
 while True:
-	json_data=open(file)
-	data = json.load(json_data)
+
+	response = urllib.urlopen(url);
+	data = json.loads(response.read())
+	#print data
+	#json_data=open(file)
+	#data = json.load(json_data)
 	temp = data["temperature"]
 	mqttc.publish(topic,temp, retain=True)
 	time.sleep(30)
